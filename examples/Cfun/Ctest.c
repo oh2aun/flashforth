b2c2d36f40b4852f4534c67dda5b3c9009d0dc3d
igor-m <igrmks@gmail.com>
2015-06-06 18:20:40 +0200

    Note on xc16-ld setting, added example

--- examples/Cfun/Ctest.c
+++ examples/Cfun/Ctest.c
@@ -1,9 +1,13 @@
 // Calling user's C function
-// IgorM 6/2015
+// Examples
 // Mind we are in XC16
-// int is 16bit
-// long int is 32bit
-// float is 32bit
+// 	int is 16bit
+// 	long int is 32bit
+// 	float is 32bit
+//
+// Note: you have to enable "Init data section" in the xc16-ld settings!
+// IgorM (c) 6.6.2015
+
 
 
 #include <stddef.h>
@@ -38,3 +42,21 @@ float Ctest(int a, int b, long int c, float x, float y)
 // decimal 100 2000 5. $0fdb $4049 $0fdb $4049 Ctest hex ok<#,ram> fa36 3eea
 // $0fdb $4049 2constant pi
 // decimal 100 2000 5. pi pi Ctest hex = 0x3eeafa36 = 4.5894021E-1
+
+
+/* float Ctest(short a, short b, long int c, float x, float y)
+{
+    float temp;
+    a = a + 100;
+    b = b / a;
+    c = c * b;
+    x = sqrtf((float)c - x);
+    temp = sinf(x);
+    y = y / x * temp;
+    return y; 
+}
+*/
+// decimal 900 -20000 -5. pi pi Ctest hex  ok<$,ram> 5b81 be04
+// -1.292553E-1
+
+
