514e43566a3f3b7504e9ef78eff16c9d751b4593
igor-m <igrmks@gmail.com>
2015-06-10 17:39:14 +0200

    Edit.

--- examples/Cfun/Cdistbear1.c
+++ examples/Cfun/Cdistbear1.c
@@ -1,11 +1,16 @@
 // Distance and Bearing demo 1
-// Example for FlashForth usinf External Cfunctions
-// This is called via Cdistbear.s
-// We get 2 results with single return from the C function !!!
-// Calculates distance in meters and initial bearing in degrees based on
-// latidute and longitude (decimal) of two points on Earth.
+// Example for FlashForth using an External Cfunction call
+//
+// This Cfunction is called via Cdistbear1.s
+// We get 2 results with a single return from the C function !!
+//
+// Calculates distance in meters and initial bearing in degree based on
+// latidute and longitude (decimal degree) of two points on Earth.
+//
+// distbear (lat1 lon1 lat2 lon2 -- bearing distance)   \ all floats
+//
 // Uses the Haversine formula
-// c IgorM 6/2015
+// (c) IgorM 10.6.2015
 // GNU GPL v3
 // No warranties of any kind
 // Provided as-is
@@ -24,27 +29,28 @@ union retData
 
 double Cdistbear (float lat1, float lon1, float lat2, float lon2) {
 
-    union retData result;
+	union retData result;
 
-double phi1 = (double)lat1 * PI / 180.0;
-double phi2 = (double)lat2 * PI / 180.0;
-double dphi = ((double)lat2 - (double)lat1) * PI / 180.0;
-double dlambda = ((double)lon2 - (double)lon1)* PI / 180.0;
+	double phi1 = (double)lat1 * PI / 180.0;
+	double phi2 = (double)lat2 * PI / 180.0;
+	double dphi = ((double)lat2 - (double)lat1) * PI / 180.0;
+	double dlambda = ((double)lon2 - (double)lon1)* PI / 180.0;
 
-double a1 = sin(dphi/2.0)*sin(dphi/2.0);
-double a2 = cos(phi1)*cos(phi2);
-double a3 = sin(dlambda/2.0)*sin(dlambda/2.0);
-double a = a1 + (a2 * a3);
-double c = 2.0 * atan2(sqrt(a), sqrt(1.0-a));
-double dis = R * c;
+	double a1 = sin(dphi/2.0)*sin(dphi/2.0);
+	double a2 = cos(phi1)*cos(phi2);
+	double a3 = sin(dlambda/2.0)*sin(dlambda/2.0);
+	double a = a1 + (a2 * a3);
+	double c = 2.0 * atan2(sqrt(a), sqrt(1.0-a));
+	double dis = R * c;
 
-double y = sin(dlambda)*cos(phi2);
-double x = cos(phi1)*sin(phi2)-sin(phi1)*cos(phi2)*cos(dlambda);
-double brn = (atan2(y,x)) * 180.0 / PI;
+	double y = sin(dlambda)*cos(phi2);
+	double x = cos(phi1)*sin(phi2)-sin(phi1)*cos(phi2)*cos(dlambda);
+	double brn = (atan2(y,x)) * 180.0 / PI;
 
-result.i[0] = dis;
-result.i[1] = brn;
-return result.j;
+	result.i[0] = dis;	// create 2 floats result
+	result.i[1] = brn;
+
+	return result.j;	// return 2 floats via 1 double
 }
 
 // lat1  lon1   lat2     lon2 (degree)
