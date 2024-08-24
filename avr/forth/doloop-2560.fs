d35047bc58b9df9bd254955f785cb9b5f3137a4d
Mikael Nordman <oh2aun@gmail.com>
2024-08-24 08:22:22 +0300

    ATMEGA: Optimized I J for DO..LOOP

--- avr/forth/doloop-2560.fs
+++ avr/forth/doloop-2560.fs
@@ -1,7 +1,7 @@
 \ *********************************************************************
 \                                                                     *
 \    Filename:      doloop.fs                                         *
-\    Date:          22.08.2024                                        *
+\    Date:          24.08.2024                                        *
 \    File Version:  5.0                                               *
 \    MCU:           Atmega 2560 and similar                           *
 \    Copyright:     Mikael Nordman                                    *
@@ -17,14 +17,14 @@ marker -doloop
 #20 constant ind inlined   \ R20:R21 index
 
 : (do)  ( limit index -- R: leave oldindex xfaxtor )
-  [ $910f i, ] \ pop unused zero address byte
+  [ $910f i, ]   \ pop unused zero address byte
   r>
   dup >a xa> @ >r            \ R: leave 
   ind @ >r                   \ R: leave oldindex
   swap $8000 swap - dup >r   \ R: leave oldindex xfactor
   + ind !
   a> 1+ >r
-  [ $925f i, ] \ push unused zero address byte
+  [ $925f i, ]  \ push unused zero address byte
 ; compileonly
 
 : (?do) ( limit index -- R: leave oldindex xfactor ) 
@@ -32,64 +32,83 @@ marker -doloop
   if
     [ '  (do) ] again  \ branch to (do) 
   then
-  [ $910f i, ] r> xa> @ >r [ $925f i, ] 2drop
-; compileonly
-
-: (+loop) ( n -- )
-  [ $0f48 i, ]   \ add r20, tosl
-  [ $1f59 i, ]   \ add r21, tosh
-  inline drop
+  [ $910f i, ] r> xa> @ >r [ $925f i, ] drop drop
 ; compileonly
 
 : unloop
-  [ $910f i, ] r>
-  rdrop r> ind ! rdrop
-  >r [ $925f i, ]
+  [ $910f i, ] r>  \ remember return address
+  rdrop            \ xfactor 
+  r> ind !         \ old index
+  rdrop            \ leave address
+  >r [ $925f i, ]  \ restore return address
 ; compileonly
 
 : do
   postpone (do)
-  postpone begin
-  flash 2 allot ram  \ leave address
-  postpone begin
+  postpone begin     \ leave storage address
+  flash 2 allot ram  \ leave address storage
+  postpone begin     \ jump back address
 ; immediate compileonly
 
 : ?do
   postpone (?do)
   postpone begin
-  flash 2 allot ram  \ leave address
+  flash 2 allot ram
   postpone begin
 ; immediate compileonly
 
 : leave
-  [ $910f i, ] rdrop
-  rdrop r> ind !
-  [ $925f i, ]
+  [ $910f i, ] rdrop  \ drop the return address
+  rdrop               \ xfactor
+  r> ind !            \ old index
+  [ $925f i, ]        \ return to leave address 
 ; compileonly
 
 : i
-  ind @ rp@ #4 + @ >< -
+  dup           \ st    -y, r25
+                \ st    -y, r24
+  [ $b7ed i, ]  \ in    r30, 3d
+  [ $b7fe i, ]  \ in    r31, 3e
+  [ $9634 i, ]  \ adiw  r30, 4
+  [ $9191 i, ]  \ ld    r25, z+
+  [ $9181 i, ]  \ ld    r24, z+   fetch xfactor
+  [ $018a i, ]  \ movw  r16, r20  fetch index
+  [ $1b08 i, ]  \ sub   r16, r24
+  [ $0b19 i, ]  \ sbc   r17, r25  index-xfactor
+  [ $01c8 i, ]  \ movw  r24, r16
 ; compileonly
 
 : j
-  rp@ #6 + @ >< rp@ #10 + @ >< - 
+  dup           \ st    -y, r25
+                \ st    -y, r24
+  [ $b7ed i, ]  \ in    r30, 3d
+  [ $b7fe i, ]  \ in    r31, 3e
+  [ $9636 i, ]  \ adiw  r30, 6
+  [ $9111 i, ]  \ ld    r17, z+
+  [ $9101 i, ]  \ ld    r16, z+   fetch index
+  [ $9632 i, ]  \ adiw  r30, 2
+  [ $9191 i, ]  \ ld    r25, z+
+  [ $9181 i, ]  \ ld    r24, z+   fetch xfactor
+  [ $1b08 i, ]  \ sub   r16, r24
+  [ $0b19 i, ]  \ sbc   r17, r25  index-xfactor
+  [ $01c8 i, ]  \ movw  r24, r16
 ; compileonly
 
-
 : loop
-  $0d46 i, $1d55 i, \ add 1 to r20:r21
-\  postpone (loop)
-  $f00b i,               \ bra +2 if overflow
+  $0d46 i, $1d55 i,       \ add 1 to r20:r21
+  $f00b i,                \ bra +2 if overflow
   postpone again
   postpone unloop
-  flash here >xa swap ! ram
+  flash here >xa swap ! ram \ leave address stored after (do)
 ; immediate compileonly
 
 : +loop
-  postpone (+loop)
+  $0f48 i,               \ add r20, tosl
+  $1f59 i,               \ add r21, tosh
+  $9189 i, $9199 i,      \ drop
   $f00b i,               \ bra +2 if overflow
   postpone again
   postpone unloop
-  flash here >xa swap ! ram
+  flash here >xa swap ! ram \ leave address stored after (?do)
 ; immediate compileonly
 
--- avr/forth/doloop.fs
+++ avr/forth/doloop.fs
@@ -1,7 +1,7 @@
 \ *********************************************************************
 \                                                                     *
 \    Filename:      doloop.fs                                         *
-\    Date:          22.08.2024                                        *
+\    Date:          24.08.2024                                        *
 \    File Version:  5.0                                               *
 \    MCU:           Atmega (not 2560)                                 *
 \    Copyright:     Mikael Nordman                                    *
@@ -34,12 +34,6 @@ marker -doloop
   r> xa> @ >r 2drop
 ; compileonly
 
-: (+loop) ( n -- )
-  [ $0f48 i, ]   \ add r20, tosl
-  [ $1f59 i, ]   \ add r21, tosh
-  inline drop
-; compileonly
-
 : unloop
   r>
   rdrop r> ind ! rdrop
@@ -65,17 +59,37 @@ marker -doloop
 ; compileonly
 
 : i
-  ind @ rp@ 3 + @ >< -
+  dup           \ st    -y, r25
+                \ st    -y, r24
+  [ $b7ed i, ]  \ in    r30, 3d
+  [ $b7fe i, ]  \ in    r31, 3e
+  [ $9633 i, ]  \ adiw  r30, 3
+  [ $9191 i, ]  \ ld    r25, z+
+  [ $9181 i, ]  \ ld    r24, z+   fetch xfactor
+  [ $018a i, ]  \ movw  r16, r20  fetch index
+  [ $1b08 i, ]  \ sub   r16, r24
+  [ $0b19 i, ]  \ sbc   r17, r25  index-xfactor
+  [ $01c8 i, ]  \ movw  r24, r16
 ; compileonly
 
 : j
-  rp@ 5 + @ >< rp@ 9 + @ >< - 
+  dup           \ st    -y, r25
+                \ st    -y, r24
+  [ $b7ed i, ]  \ in    r30, 3d
+  [ $b7fe i, ]  \ in    r31, 3e
+  [ $9635 i, ]  \ adiw  r30, 5
+  [ $9111 i, ]  \ ld    r17, z+
+  [ $9101 i, ]  \ ld    r16, z+   fetch index
+  [ $9632 i, ]  \ adiw  r30, 2
+  [ $9191 i, ]  \ ld    r25, z+
+  [ $9181 i, ]  \ ld    r24, z+   fetch xfactor
+  [ $1b08 i, ]  \ sub   r16, r24
+  [ $0b19 i, ]  \ sbc   r17, r25  index-xfactor
+  [ $01c8 i, ]  \ movw  r24, r16
 ; compileonly
 
-
 : loop
-  $0d46 i, $1d55 i, \ add 1 to r20:r21
-\  postpone (loop)
+  $0d46 i, $1d55 i,      \ add 1 to r20:r21
   $f00b i,               \ bra +2 if overflow
   postpone again
   postpone unloop
@@ -83,7 +97,9 @@ marker -doloop
 ; immediate compileonly
 
 : +loop
-  postpone (+loop)
+  $0f48 i,               \ add r20, tosl
+  $1f59 i,               \ add r21, tosh
+  $9189 i, $9199 i,      \ drop
   $f00b i,               \ bra +2 if overflow
   postpone again
   postpone unloop
